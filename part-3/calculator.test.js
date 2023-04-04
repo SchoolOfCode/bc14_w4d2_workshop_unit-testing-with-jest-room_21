266d6eb277c57f24394bf4dc3369723e852b31ef
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-04-04 16:13:34 +0100

    adding tests complete

--- part-3/calculator.test.js
+++ part-3/calculator.test.js
@@ -1,1 +1,28 @@
+import { test, expect } from '@jest/globals';
+import { add, subtract, multiply, divide, square, calculate } from './calculator.js';
 
+test('adding 1 and 2 should return 3', function () {
+// Arrange
+// variables
+const actual = add(1, 2);
+const expected = 3;
+
+// Act 
+
+//Assert
+expect(actual).toBe(expected);
+
+});
+
+test('adding -11 and 6 should return -5', function () {
+    // Arrange
+    // variables
+    const actual = add(-11, 6);
+    const expected = -5;
+    
+    // Act 
+    
+    //Assert
+    expect(actual).toBe(expected);
+    
+    });
