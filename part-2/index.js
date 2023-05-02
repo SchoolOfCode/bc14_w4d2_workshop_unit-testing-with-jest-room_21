5e1abdfe5067f3345d78327dc8ec4de68b06c3df
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-05-02 15:38:54 +0100

    task 1 and task completed

--- part-2/index.js
+++ part-2/index.js
@@ -1,6 +1,16 @@
 /* Complete the following greetPerson function so that it passes 
-the tests in index.test.js. */
+the tests in index.test.js. 
+
+String goes in as an argument -> "Greetings, Liz K" as the return value
+
+Number or boolean goes in -> "Please try again with a name!"
+
+*/
 
 export function greetPerson(name) {
-  // Write the code for this function based on what you see in the tests!
+  if (typeof name === "string") {
+    return `Greetings, ${name}`;
+  } else {
+    return "Please try again with a name!";
+  }
 }
